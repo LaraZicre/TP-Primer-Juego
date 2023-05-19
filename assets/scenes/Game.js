924786c9beed43ea52d2905b1dd0af76e4ca9816
LaraZicre <l.zicre@gmail.com>
2023-05-19 10:11:12 -0300

    mejoras y edicion

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -58,6 +58,8 @@ export default class Game extends Phaser.Scene {
     this.platformsGroup.create(200, 250, "platformmini").setScale(1).refreshBody();
     this.platformsGroup.create(600, 300, "platformmini").setScale(1).refreshBody();
     this.platformsGroup.create(320, 420, "platformmini").setScale(1).refreshBody();
+
+    
     // add shapes group
     this.shapesGroup = this.physics.add.group();
 
@@ -145,16 +147,6 @@ export default class Game extends Phaser.Scene {
     }
   }
 
-  // Desarrollar un videojuego que, inicialmente, cuente con una plataforma inferior, donde se posicione el jugador.
-
-  // Desde la parte superior de la pantalla deberán caer items recolectables en forma de cuadrados, triángulos y rombos, de forma aleatoria con intervalos de un segundo.
-
-  // Condición para GANAR: juntar al menos 2 ítems de cada tipo. La acumulación de objetos se deberá persistir en un estructura de datos “array”. Al momento de recolectar un nuevo elemento se deberá verificar la condición de victoria.
-  // Mejora 1: Adicionar un contador de tiempo descendente. Al llegar a 0, el jugador PIERDE.
-  // Mejora 2: Asignar un puntaje distinto a cada elemento recolectable. Condición de GANAR: superar los 100 puntos.
-  // Mejora 3: Reducir intervalo entre cada aparición de un nuevo item a 0.5 segundos. En cada rebote con el piso, descontar 1 punto al objeto. Si llega a 0, desaparece.
-  // Mejora 4: Agregar más plataformas a la escena.
-  // Mejora 5: Agregar un nuevo tipo de objeto que al recolectar descuenta puntos.
 
   collectShape(jugador, figuraChocada) {
     // remove shape from screen
